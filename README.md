8e9c11cefa151023a3d7523ad214e12623b5aed7
Rogerio Hanssen <ahanssen@gmail.com>
2020-08-21 21:58:57 -0300

    Novo tema

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # flappy-doc
+
+Image credits
+coronavirus by Pham Thanh Lôc from the Noun Project
+mask by KonKapp from the Noun Project
+sanitizer by Komkrit Noenpoempisut from the Noun Project
--- flappy-doc.js
+++ flappy-doc.js
@@ -30,7 +30,8 @@ const background = {
     x: 0,
     y: canvas.height - 204,
     draw() {
-        context.fillStyle = '#70c5ce';
+        // context.fillStyle = '#70c5ce';
+        context.fillStyle = '#cecece';
         context.fillRect(0,0, canvas.width, canvas.height);
 
         context.drawImage(
