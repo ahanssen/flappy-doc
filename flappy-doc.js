ef26edf890978cf2f0eac7d79f4e05217d0033bf
Rogerio A Hanssen <ahanssen@users.noreply.github.com>
2023-09-09 17:04:16 -0300

    Update flappy-doc.js increase vertical gap to 250

--- flappy-doc.js
+++ flappy-doc.js
@@ -206,7 +206,7 @@ function pipes() {
         draw() {
             pipes.doublePipes.forEach(function(pipe) {
                 const yRandom = pipe.y;
-                const verticalGap = 150;
+                const verticalGap = 250;
 
                 const skyPipeX = pipe.x;
                 const skyPipeY = yRandom;
